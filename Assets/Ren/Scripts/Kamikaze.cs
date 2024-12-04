e8129b8e05d897651b4ff5d46c5633f416eb7e46
Renscal <Malik@fam-kjaer.dk>
2024-12-04 14:03:18 +0100

    d

--- Assets/Ren/Scripts/Kamikaze.cs
+++ Assets/Ren/Scripts/Kamikaze.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class Kamikaze : MonoBehaviour {
     [Header("Gameobjects")]
     public GameObject player;
     public GameObject visualBody;
-    public GameObject explotion;
+    public GameObject explotion1;
+    public GameObject explotion2;
+    public GameObject fuse;
 
     [Header("Configurations")]
     public float speed;
+    bool dead;
 
     Vector3 playerCoords;
 
     // Gamecomponents
-    ParticleSystem particles;
+    ParticleSystem particles1;
+    ParticleSystem particles2;
+    ParticleSystem fuseParticles;
     Transform playerTR;
     Transform TR;
     AudioSource AS;
@@ -24,19 +30,28 @@ public class Kamikaze : MonoBehaviour {
         playerTR = player.GetComponent<Transform>();
         TR = GetComponent<Transform>();
         AS = GetComponent<AudioSource>();
-        particles = explotion.GetComponent<ParticleSystem>();
+        particles1 = explotion1.GetComponent<ParticleSystem>();
+        particles2 = explotion2.GetComponent<ParticleSystem>();
+        fuseParticles = fuse.GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update() {
-        playerCoords = new Vector3(playerTR.position.x, TR.position.y, playerTR.position.z);
-        TR.LookAt(playerCoords);
-        TR.Translate(0, 0, speed * Time.deltaTime);
+        if(dead == false) {
+            playerCoords = new Vector3(playerTR.position.x, TR.position.y, playerTR.position.z);
+            TR.LookAt(playerCoords);
+            TR.Translate(0, 0, speed * Time.deltaTime);
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
-        particles.Play();
-        visualBody.SetActive(false);
-        AS.Play();
+        if (other.tag == "Player" && dead == false) {
+            particles1.Play();
+            particles2.Play();
+            visualBody.SetActive(false);
+            AS.Play();
+            dead = true;
+            fuseParticles.Stop();
+        }
     }
 }
