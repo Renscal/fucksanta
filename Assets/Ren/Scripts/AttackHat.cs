89836ef6c30cfb6468beaca21bd5f7e39d100f63
Renscal <Malik@fam-kjaer.dk>
2024-12-05 10:58:41 +0100

    Santa be packin'

--- Assets/Ren/Scripts/AttackHat.cs
+++ Assets/Ren/Scripts/AttackHat.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class AttackHat : MonoBehaviour {
     [Header("Gameobjects")]
     public GameObject attackHat;
+    public GameObject attackHatDad;
     public GameObject santaHat;
 
     [Header("Configurations")]
@@ -13,21 +15,23 @@ public class AttackHat : MonoBehaviour {
     //Gamecomponents
     Transform santaHatTF;
     Transform attackHatTF;
+    Transform attackHatDadTF;
     Animator attackHatAnim;
 
 
     void Start() {
         santaHatTF = santaHat.GetComponent<Transform>();
         attackHatTF = attackHat.GetComponent<Transform>();
+        attackHatDadTF = attackHatDad.GetComponent<Transform>();
         attackHatAnim = attackHat.GetComponent<Animator>();
+        attackHat.SetActive(true);
     }
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.O)) {
             santaHat.SetActive(false);
             attackHat.SetActive(true);
-            attackHatTF.position = new Vector3(4.58297f, 55.9905f, 20.66323f);
-            attackHatTF.rotation = new Quaternion(0, santaHatTF.rotation.y, 0, 0);
+            attackHatDadTF.rotation = new Quaternion(0, santaHatTF.rotation.y, 0, 0);
             attackHatAnim.SetTrigger("Attack");
         }
     }
